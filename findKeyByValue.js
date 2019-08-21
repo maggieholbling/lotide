2c4428e3fe5944cb6b9196a48f7d53bf45ea9a87
Maggie <maggieholbl@gmail.com>
2019-08-21 22:31:52 +0000

    initial commit

---
+++ findKeyByValue.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🤕Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(ourObject, valueOfKey) {
+  let result = undefined;
+  let count = 0;
+  for (const key in ourObject) {
+    if (ourObject[key] === valueOfKey) {
+      result = Object.keys(ourObject)[count];
+      return result;
+    }
+    count ++;
+  }
+  return result;
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire",
+  thriller:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
