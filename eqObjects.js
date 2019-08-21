61dd6e1872ccacdccbe53e4996785df239060c39
Maggie <maggieholbl@gmail.com>
2019-08-21 23:30:59 +0000

    linted code

--- eqObjects.js
+++ eqObjects.js
@@ -25,7 +25,7 @@ const eqObjects = function(object1, object2) {
   if (Object.keys(object1).length === Object.keys(object2).length) {
     for (const key in object1) {
       if (!Array.isArray(object1[key])) {
-        if(object1[key] === object2[key]) {
+        if (object1[key] === object2[key]) {
           result = true;
         } else {
           result = false;
