b82b66654ad1cec76f1238ab5fe1b7e492364a80
Maggie <maggieholbl@gmail.com>
2019-08-21 01:13:02 +0000

    fixed syntax

--- middle.js
+++ middle.js
@@ -20,18 +20,18 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
-const middle = function (array) {
+const middle = function(array) {
   let middleArray = [];
-  let middleIndex = array.length/2;
+  let middleIndex = array.length / 2;
   if (array.length > 2) {
     if (array.length % 2 === 0) {
-      middleArray.push(array[middleIndex-1], array[middleIndex]);
+      middleArray.push(array[middleIndex - 1], array[middleIndex]);
     } else {
       middleArray.push(array[Math.floor(middleIndex)]);
     }
   }
   return middleArray;
-}
+};
 
 assertArraysEqual(middle([3,4,6,2,5]), [6]);
 assertArraysEqual(middle([3,4,2,5]), [4,2]);
