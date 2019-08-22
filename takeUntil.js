bbb7c99c53d14ad90ad7960496fd80a39c91d291
Maggie <maggieholbl@gmail.com>
2019-08-22 22:11:23 +0000

    initial commit

---
+++ takeUntil.js
@@ -0,0 +1,43 @@
+const takeUntil = function(array, callback) {
+  let result = [];
+  for (let elem of array) {
+    if (!callback(elem)) {
+      result.push(elem);
+    } else {
+      break;
+    }
+  }
+  return result;
+};
+
+const eqArrays = function(array1, array2) {
+  let isTrue = false;
+  if (array1.length === array2.length) {//check if arrays are equal length
+    for (let i = 0; i < array1.length; i++) {//loop for the length of array1
+      if (array1[i] !== array2[i]) {//compare first and last elements
+        return false;
+      } else {
+        isTrue = true;
+      }
+    }
+  }
+  return isTrue;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🤕Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+const results3 = takeUntil(data2, x => x.length === 20);
+
+assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
+assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
+assertArraysEqual(results3, ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"]);
