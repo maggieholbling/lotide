389304303a1336c2da6d19c00625b88b84f8a6b3
Maggie <maggieholbl@gmail.com>
2019-08-26 19:59:26 +0000

    refactored with an initial test file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,6 +1,5 @@
-const eqArrays = require('./eqArrays');
-
 const assertArraysEqual = function(actual, expected) {
+  const eqArrays = require('./eqArrays');
   if (eqArrays(actual, expected)) {
     console.log(`✅Assertion Passed: ${actual} === ${expected}`);
   } else {
@@ -9,6 +8,3 @@ const assertArraysEqual = function(actual, expected) {
 };
 
 module.exports = assertArraysEqual;
-
-// assertArraysEqual([1,2,3], [1,2,3]);
-// assertArraysEqual([1,2,3], [1,"2",3]);
