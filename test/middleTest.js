09b29013cc2ff1b59ea629b1af51ad81e9b30865
Maggie <maggieholbl@gmail.com>
2019-08-26 20:58:38 +0000

    converted to mocha test

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,24 @@
-const assertArraysEqual = require('../assertArraysEqual');
+// const assertArraysEqual = require('../assertArraysEqual');
+// const middle = require('../middle');
+// assertArraysEqual(middle([3,4,6,2,5]), [6]);
+// assertArraysEqual(middle([3,4,2,5]), [4,2]);
+// assertArraysEqual(middle(["3","4","6","2","5"]), ["6"]);
+// assertArraysEqual(middle(["3","4","2","5"]), ["4","2"]);
+
+const assert = require('chai').assert;
 const middle = require('../middle');
-assertArraysEqual(middle([3,4,6,2,5]), [6]);
-assertArraysEqual(middle([3,4,2,5]), [4,2]);
-assertArraysEqual(middle(["3","4","6","2","5"]), ["6"]);
-assertArraysEqual(middle(["3","4","2","5"]), ["4","2"]);
+
+describe("#middle", () => {
+  it("returns '[6]' for the input '[3,4,6,2,5]'", () => {
+    assert.deepEqual(middle([3,4,6,2,5]), [6]); 
+  });
+  it("returns '[4,2]' for the input '[3,4,2,5]'", () => {
+    assert.deepEqual(middle([3,4,2,5]), [4,2]); 
+  });
+  it("returns '['6']' for the input '['3','4','6','2','5']'", () => {
+    assert.deepEqual(middle(['3','4','6','2','5']), ['6']); 
+  });
+  it("returns '['4','2']' for the input '['3','4','2','5']'", () => {
+    assert.deepEqual(middle(['3','4','2','5']), ['4','2']); 
+  });
+});
